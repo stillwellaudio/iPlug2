6d49baf953c15e1a8b583adac6503c4b56f1f611
Scott Stillwell <scott@stillwellaudio.com>
2021-05-15 00:00:24 -0500

    Revert "Fix missed #ifdef for consistent declaration of SetScreenScale"
    This reverts commit 3c830a1938e5372d47e6ee9f7e00cf7cf233ff7d.

--- IGraphics/IGraphics.cpp
+++ IGraphics/IGraphics.cpp
@@ -76,11 +76,7 @@ IGraphics::~IGraphics()
   svgStorage.Release();
 }
 
-#ifdef IGRAPHICS_QUANTISE_SCREENSCALE
-void IGraphics::SetScreenScale(int scale)
-#else
 void IGraphics::SetScreenScale(float scale)
-#endif
 {
   mScreenScale = scale;
   int windowWidth = WindowWidth() * GetPlatformWindowScale();
